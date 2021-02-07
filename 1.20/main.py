c8541ad84739c0aabaad1c829b8a9ca912bbab82
BrianPavillarCIS <77904356+BrianPavillarCIS@users.noreply.github.com>
2021-02-07 13:17:38 -0600

    Add files via upload

---
+++ 1.20/main.py
@@ -0,0 +1,14 @@
+# Brian Pavillar
+# ID: 1863509
+
+user_num1 = int(input("Enter integer:\n"))
+print("You entered:", user_num1)
+user_num2 = user_num1 * user_num1
+print(user_num1, "squared is", user_num2)
+user_num2 = user_num2 * user_num1
+print("And", user_num1, "cubed is", user_num2, "!!")
+user_num3 = int(input("Enter another integer:\n"))
+user_num4 = user_num1 + user_num3
+print(user_num1, "+", user_num3, "is", user_num4)
+user_num5 = user_num1 * user_num3
+print(user_num1, "*", user_num3, "is", user_num5)
