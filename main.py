68646b1c8060d222b4d94cee8a166338dbcbdaa7
BrianPavillarCIS <77904356+BrianPavillarCIS@users.noreply.github.com>
2021-02-07 13:02:01 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Brian Pavillar
+# ID: 1863509
+
+# Attempted math import (Import doesnt seem to work in PyCharm)
+# import math
+# userNum = int(input())
+# userNumSquared = userNum.pow(userNum, 2)
+# print(userNumSquared)
+
+# Other Exponential Method
+
+userNum = int(input())
+userNumSquared = userNum ** 2
+print(userNumSquared)
+
